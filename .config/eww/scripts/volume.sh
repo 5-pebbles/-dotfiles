b6b41a50efe2d734de494e9ea74334deca658b34
5-pebbles <5-pebble@protonmail.com>
2024-02-07 03:08:24 +0000

    feat(hypr&eww): lots of stuff, no I don't want to talk about it.

--- .config/eww/scripts/volume.sh
+++ .config/eww/scripts/volume.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-percent=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d\n", $2 * 100 + 0.5}')
+percent=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d", $2 * 100 + 0.5}' | tr -d '[]')
 
 if [ "$1" = "icon" ]; then
     if [ $percent = 0 ]; then
